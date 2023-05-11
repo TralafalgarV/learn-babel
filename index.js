cf151e0e9e20e2f32b5627b0d1f1bdc53533f753
wangwei1 <578895238@qq.com>
2023-05-11 09:36:26 +0800

    feat：添加insert插件

---
+++ index.js
@@ -0,0 +1,13 @@
+const { transformFileSync } = require("@babel/core");
+const insertParametersPlugin = require("./plugin-insert.js");
+const path = require("path");
+
+const { code } = transformFileSync(path.join(__dirname, "./source.js"), {
+  plugins: [insertParametersPlugin],
+  parserOpts: {
+    sourceType: "unambiguous",
+    plugins: ["jsx"],
+  },
+});
+
+console.log(code);
